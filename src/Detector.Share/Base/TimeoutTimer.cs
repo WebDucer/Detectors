2d74cda52a6d4016eec4202897093320c83af3aa
Eugen Richter <android@webducer.de>
2017-03-27 21:07:59 +0200

    Prepare for NetStandard implementation

---
+++ src/Detector.Share/Base/TimeoutTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace de.webducer.net.Detector.Base
+{
+	public class TimeoutTimer : CancellationTokenSource, IDisposable
+	{
+		public TimeoutTimer(TimeSpan timeOut, Action callback)
+		{
+			if (timeOut.TotalMilliseconds < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(timeOut), "Timeout should be always positive");
+			}
+
+			Task.Delay(timeOut)
+				.ContinueWith(t => callback(),
+			                  CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously
+			                  | TaskContinuationOptions.OnlyOnRanToCompletion,
+			                  TaskScheduler.Default);
+		}
+
+		public new void Dispose()
+		{
+			base.Cancel();
+		}
+	}
+}
